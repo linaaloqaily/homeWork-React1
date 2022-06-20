69c20d87f4233159b9fa19dc71b52b501aa8c857
LINA <91501725+linaaloqaily@users.noreply.github.com>
2022-06-20 22:35:56 +0300

    v1

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../App.css";
+import Cat from "./Cat";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Cats</h1>
+      <Cat />
+    </div>
+  );
+}
+
+export default App;
